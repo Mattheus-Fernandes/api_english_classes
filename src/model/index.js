0cf2932d4ef386ffabc03687483ddfd07d419154
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-29 17:39:34 -0300

    created model

---
+++ src/model/index.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize")
+const connection = require("../database/index")
+
+const Class = connection.connection.define("class", {
+  student: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  language: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  unit: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  data: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  hour: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+
+Class.sync({force: false})
+  .then(() => {})
+
+module.exports = Class

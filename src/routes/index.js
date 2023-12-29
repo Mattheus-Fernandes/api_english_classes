6e4cfa2ec4daa8b8f654dde02678465823dc2315
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-29 17:41:51 -0300

    setting express

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router()
 const Controller = require("../controller/index")
 
 router.get("/class", Controller.getAll)
+router.post("/class", Controller.saveClass)
 
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -4,9 +4,13 @@ const router = require("./routes/index")
 const connection = require("./database/index")
 const ClassModel = require("./model/index")
 
-
+//Setting express
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use("/", router)
 
+
+//Connection with database
 connection.connect()
 
 const port = 8080 || process.env

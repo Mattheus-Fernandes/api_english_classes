a272cb9fbb8ae974f5c7029749f981e4d8351b8d
Mattheus Fernandes <mattheus.ale@hotmail.com>
2023-12-29 17:40:26 -0300

    created function saveClass

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,8 +1,27 @@
+const Class = require("../model/index")
+
 function getAll (req, res) {
-  res.json({message: "Ok"})
+
+  Class.findAll({raw: true})
+    .then(classes => res.json(classes))
+
+}
+
+function saveClass(req, res) {
+
+  const {student, hour, language, data, unit} = req.body
+  const unitValue = `Unidade ${unit}`
+
+  Class.create({
+    student, language, unit: unitValue, data, hour
+  }).then(() => {
+    res.json("created with success")
+  })
+
 }
 
 
 module.exports = {
   getAll,
+  saveClass
 }

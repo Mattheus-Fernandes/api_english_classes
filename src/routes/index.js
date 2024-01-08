d81eb0a2669e2d7cff9cb0a864c2f0f04e1f5eec
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-01-08 15:48:22 -0300

    create new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,12 @@ const router = require("express").Router()
 const Controller = require("../controller/index")
 
 router.get("/class", Controller.getAll)
+router.get("/class/:student", Controller.getStudent)
 router.post("/class", Controller.saveClass)
+router.put("/class/:id", Controller.editStudent)
+router.delete("/class/:id", Controller.deleteStudent)
+
+
 
 
 module.exports = router

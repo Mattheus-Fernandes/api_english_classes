7d300296e40d74f049bc445575dd77c7ec13df39
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-01-08 15:49:48 -0300

    created function async/await getAll

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,30 +1,107 @@
 const Class = require("../model/index")
 
-function getAll (req, res) {
+async function getAll (req, res) {
 
-  Class.findAll({raw: true, order: [
-    ["id", "desc"]
-  ]})
-    .then(classes => res.json(classes))
-
+  try {
+    const results = await Class.findAll({raw: true, order: [["id", "desc"]]})
+    
+    if(results){
+      res.json(results)
+    }else {
+      res.json({message: "Nenhum aluno encontrado"})
+    }
+  }catch(error){
+    res.json({error: "Ocorreu um erro..."})
+  }
 }
 
-function saveClass(req, res) {
+async function getStudent (req, res) {
+  
+  const { student } = req.params
 
-  const {student, hour, language, data, unit} = req.body
+  try {
+    if(student != undefined) {
+      const results = await Class.findOne({where: {student}})
+      
+      if(results){
+        res.json(results)
+      }else {
+        res.json({message: "Aluno não encontrado"})
+      }
+    }
+  }catch(error){
+    res.json({error: "Ocorreu um erro..."})
+  }
+}
+
+async function saveClass(req, res) {
+
+  const {student, hour, language, unit} = req.body
   const unitValue = `Unidade ${unit}`
 
 
-  if(student !== " " && language !== " " && unit !== " " &&  data !== " " && hour !== " ") {
+  try{
+    if(student !== " " && language !== " " && unit !== " " && hour !== " ") {
+      const results = await Class.create({student, language, unit: unitValue, hour})
 
-    Class.create({
-      student, language, unit: unitValue, data, hour
-    }).then(() => {
-      res.json({message: "success"})
-    })
-    
-  }else{
-    res.json("Por favor preencha todos os dados")
+      if(results) {
+        res.json({message: "Aula registrada com sucesso!"})
+      }
+    }else{
+      res.json({message: "Por favor preencha todos os dados!"})
+    }
+  }catch(error){
+    res.json({error: "Ocorreu um erro..."})
+  }
+}
+
+async function editStudent (req, res) {
+  
+  const { id } = req.params
+  const idNumber = parseInt(id)
+  const {student, hour, language, unit} = req.body
+  const unitValue = `Unidade ${unit}`
+
+
+  try{
+    if(idNumber){
+      if(student !== " " && language !== " " && unit !== " "  && hour !== " ") {
+          const results = await Class.update({student, language, unit: unitValue, hour}, {where: {id}})
+          
+
+          if(results == false){
+            res.json({message: "Aluno não encontrado"})
+          }else{
+            res.json({message: "Alterações realizadas com sucesso!"})
+          }
+
+      }else{
+        res.json({message: "Por favor preencha todos os dados!"})
+      } 
+
+    }
+  }catch(error){
+    res.json({error: "Insira somente números"})
+  }
+}
+
+async function deleteStudent (req, res){
+
+  const { id } = req.params
+  const idNumber = parseInt(id)
+
+  try {
+    if(idNumber != undefined){
+      const results = await Class.destroy({where: {id}})
+      
+      if(results){
+        res.json({message: "Aula excluída!"})
+      }else {
+        res.json({message: "Não foi possível excluir essa aula"})
+      }
+    }
+  }catch(error){
+    res.json({error: "Insira somente números"})
   }
 
 }
@@ -32,5 +109,8 @@ function saveClass(req, res) {
 
 module.exports = {
   getAll,
-  saveClass
+  getStudent,
+  saveClass,
+  editStudent,
+  deleteStudent
 }

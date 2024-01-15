0084c1f3900cabced6e54f44514db2454ac8d1f1
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-01-15 10:05:23 -0300

    changed params of the route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const Controller = require("../controller/index")
 
 router.get("/class", Controller.getAll)
-router.get("/class/:student", Controller.getStudent)
+router.get("/class/:id", Controller.getStudent)
 router.post("/class", Controller.saveClass)
 router.put("/class/:id", Controller.editStudent)
 router.delete("/class/:id", Controller.deleteStudent)
